e6004e7ee936e5a8f17bdbd4fc0a46a8c7865967
JorgePennaZ <elmaxxxdemon@gmail.com>
2021-07-16 22:26:35 -0500

    se elimino una clase y se agrego la clase menu

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Menu{
+	public static void MostrarMenu() throws IOException,InterruptedException{
+		System.out.println("----BIENVENIDO AL MENU DE BRO BEAR HUT----");
+		Cls();
+		System.out.println("----BIENVENIDO AL MENU DE BRO BEAR HUT----");
+
+	}
+	public static void Cls(){
+		System.out.println("\f");
+    }
+}
